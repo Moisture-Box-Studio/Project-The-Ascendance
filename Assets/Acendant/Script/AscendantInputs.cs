d428c3616ce6dd39acf946effd2e615d4e73cd93
Sebastian Rojas Benavides <bastian210@hotmail.com>
2023-07-11 00:23:29 -0500

    Deleting and Start of Character Scripting - July 10th
    Some assets were deleted for diverse issues.
    
    And the scrpting to move the character creaated for the game were started.
    
    Atte. Cbass

--- Assets/Acendant/Script/AscendantInputs.cs
+++ Assets/Acendant/Script/AscendantInputs.cs
@@ -4,15 +4,63 @@ using UnityEngine;
 
 public class AscendantInputs : MonoBehaviour
 {
+    [Header("RigidBody")]
+    Rigidbody cuerpo;
+
+    [Header("Movimiento")]
+    public bool salto = false;
+    public float fuerzaDeSalto = 5f;
+    public float velocidadFrontal = 10f;
+    public float velocidadLateral = 10f;
+
+    [Header("Rotación")]
+    public float rotacionHorizontal = 2.0f;
+    public float rotacionVertical = 2.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cuerpo = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Saltar();
+        Movimiento();
+        RotacionDeCamara();
+    }
+
+    public void Saltar()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            salto = true;
+
+            if (salto == true)
+            {
+                cuerpo.AddForce(transform.up * (fuerzaDeSalto * 50));
+            }
+        }
+    }
+
+    public void Movimiento()
+    {
+        float movimientoFrontal = Input.GetAxis("Vertical") * velocidadFrontal;
+        float movimientoLateral = Input.GetAxis("Horizontal") * velocidadLateral;
+
+        movimientoFrontal *= Time.deltaTime;
+        movimientoLateral *= Time.deltaTime;
+
+        transform.Translate(0, 0, movimientoFrontal);
+        transform.Translate(movimientoLateral, 0, 0);
+    }
+
+    public void RotacionDeCamara()
+    {
+        float rotacionEnX = rotacionHorizontal * Input.GetAxis("Mouse X");
+        float rotacionEnY = rotacionVertical * Input.GetAxis("Mouse Y");
+
+        transform.Rotate(rotacionEnY, rotacionEnX, 0);
     }
 }
--- Assets/Acendant/Script/AscendantRigidBodyPush.cs
+++ Assets/Acendant/Script/AscendantRigidBodyPush.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class AscendantRigidBodyPush : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public LayerMask empujadores;
+    public bool puedeEmpujar;
+
+    [Range(0.5f, 5f)] public float fuerza = 1.1f;
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        
+        if (puedeEmpujar) EmpujarRigidBodies(hit);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void EmpujarRigidBodies(ControllerColliderHit hit)
     {
-        
+        // Se asegura de que no choque con un cuerpo kinematico
+        Rigidbody body = hit.collider.attachedRigidbody;
+        if (body == null || body.isKinematic) return;
+
+        // Se asegura de que el empuje solo se aplica a determinadas capas o grupos
+        var bodyLayerMask = 1 << body.gameObject.layer;
+        if ((bodyLayerMask & empujadores.value) == 0) return;
+
+        // Se asegura de no empujar cuerpos debajo del jugador
+        if (hit.moveDirection.y < -0.3f) return;
+
+        // Calcula el movimiento hacia direcciones horizontales solamente 
+        Vector3 empujarEnDir = new Vector3(hit.moveDirection.x, 0.0f, hit.moveDirection.z);
+
+        // Aplica la empuje y tiene en cuenta la fuerza
+        body.AddForce(empujarEnDir * fuerza, ForceMode.Impulse);
     }
 }
